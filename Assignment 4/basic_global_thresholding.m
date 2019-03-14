ffb5a7e2c9ac0d087f8eb97f2870a336ff31abad
Yiping Wang <yiping@wang.vision>
2019-03-14 16:21:43 -0700

    imporve

--- Assignment 4/basic_global_thresholding.m
+++ Assignment 4/basic_global_thresholding.m
@@ -1,11 +1,17 @@
-function basic_global_thresholding(img, tolerance, display_hist, hist_eq)
+function basic_global_thresholding(img, tolerance, display_img_hist, hist_eq, display_binary_count_hist)
 if nargin < 3
-    display_hist = false;
+    display_binary_count_hist = false;
+    display_img_hist = false;
     hist_eq = false;
 end
 
 if nargin < 4
+    display_binary_count_hist = false;
     hist_eq = false;
+end
+
+if nargin < 5
+    display_binary_count_hist = false;
 end
 
 if hist_eq
@@ -17,42 +23,67 @@ end
 
 % Randomly select an initial estimate for T.
 row = reshape(img, numel(img), 1);
-old_T = row(randi(length(row)-1));
-mu1 = mean(img(img>old_T));
-mu2 = mean(img(img<=old_T));
-new_T = (mu1 + mu2)/2;
-disp(old_T);
-disp(new_T);
-while abs(new_T - old_T) > tolerance
-    old_T = new_T;
-    mu1 = mean(img(img>old_T));
-    mu2 = mean(img(img<=old_T));
-    new_T = (mu1 + mu2)/2;
-    disp(new_T);
-end
-    
-img = input_img>new_T;
+new_T = double(row(randi(length(row) - 1)));
+old_T = double(0);
 
-if display_hist
-    subplot(1, 2, 1);
+% Avoid divide by 0 case
+if new_T == 255
+    new_T = new_T - 1;
+elseif new_T == 0
+    new_T = new_T + 1;
+end
+
+mu1 = mean(img(img > new_T));
+mu2 = mean(img(img <= new_T));
+
+while abs(new_T - old_T) > tolerance
+    disp(new_T);
+    old_T = new_T;
+    new_T = (mu1 + mu2) / 2;
+    mu1 = mean(img(img > new_T));
+    mu2 = mean(img(img <= new_T));
+end
+
+disp(new_T);
+    
+img = input_img > new_T;
+
+% Plotting according to different arguments
+% 
+% Plot the thresholded images
+% Plot the image histogram with a read vertical specifies the threshold
+% value
+% Plot the histogram of the number binary values
+
+num_img = 1;
+if display_binary_count_hist
+    num_img = num_img + 1;
+end
+if display_img_hist
+    num_img = num_img + 1;
+    subplot(1, num_img, 1);
     imshow(img);
-    title(strcat('Threshold at Convergence: ', num2str(new_T)));
-    subplot(1, 2, 2);
+    title(strcat('Threshold at Convergence: ', num2str(new_T)), 'FontSize', 7);
+    subplot(1, num_img, 2);
+    imhist(input_img);
+    hold on;
+    line([new_T, new_T], ylim, 'LineWidth', 1, 'Color', 'r');
+else
+    subplot(1, num_img, 1);
+    imshow(img);
+    title(strcat('Threshold at Convergence: ', num2str(new_T)), 'FontSize', 12);
+end
+
+if display_binary_count_hist
+    subplot(1, num_img, num_img);
     [counts, binary_level] = imhist(img, 2);
     bar(binary_level, counts, 'BarWidth', 1);
-    if hist_eq
-        suptitle('With Histogram Equalization');
-    else
-        suptitle('Without Histogram Equalization');
-    end
+end
+    
+if hist_eq
+    suptitle('With Histogram Equalization');
 else
-    imshow(img);
-    title(strcat('Threshold at Convergence: ', num2str(new_T)));
-    if hist_eq
-        suptitle('With Histogram Equalization');
-    else
-        suptitle('Without Histogram Equalization');
-    end
+    suptitle('Without Histogram Equalization');
 end
 
 end
